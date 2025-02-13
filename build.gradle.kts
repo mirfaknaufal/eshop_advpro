79c1a3ab2318baf1817c9ff4bfbd1834e376213c
mirfaknaufal <mirfakn@gmail.com>
2025-02-13 21:59:19 +0700

    Added Functional Test

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 group = "id.ac.ui.cs.advprog"
 version = "0.0.1-SNAPSHOT"
-val seleniumJavaVersion = "4.14.1"
+val seleniumJavaVersion = "4.25.0"
 val seleniumJupiterVersion = "5.0.1"
 val webdrivermanagerVersion = "5.6.3"
 val junitJupiterVersion = "5.9.1"
